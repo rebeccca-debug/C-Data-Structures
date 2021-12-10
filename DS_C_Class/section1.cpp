32841ee7a1536901be796de82074f2559626f992
rebeccca-debug <54553333+rebeccca-debug@users.noreply.github.com>
2021-12-10 03:36:10 -0500

    Update section1.cpp

--- DS_C_Class/section1.cpp
+++ DS_C_Class/section1.cpp
@@ -17,3 +17,17 @@ main(){
     
     return 0;
 }
+
+input_output(){
+    int n;
+    cout<<"Enter Size";
+    cin>>n;
+    int A[n]={2,4,6,8,10,12};
+    
+    for(int x:A)
+    {
+        cout<<x<<endl;
+    }
+    
+    return 0;
+}
