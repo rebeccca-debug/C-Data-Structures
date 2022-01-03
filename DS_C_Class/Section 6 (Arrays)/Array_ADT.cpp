e17428ce63b889f205123bcbdf7f97b6f353ee04
rebeccca-debug <54553333+rebeccca-debug@users.noreply.github.com>
2022-01-03 16:52:08 -0500

    Update Array_ADT.cpp
    Add set operations

--- DS_C_Class/Section 6 (Arrays)/Array_ADT.cpp
+++ DS_C_Class/Section 6 (Arrays)/Array_ADT.cpp
@@ -187,6 +187,93 @@ int isSorted(struct Array arr){
     return 1;
 }
 
+
+
+
+
+//Set operations
+
+struct Array* Union(struct Array *arr1,struct Array *arr2){
+    int i,j,k;
+    i=j=k=0;
+
+    struct Array *arr3=(struct Array *)malloc(sizeof(struct
+Array));
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i]<arr2->A[j])
+            arr3->A[k++]=arr1->A[i++];
+        else if(arr2->A[j]<arr1->A[i])
+            arr3->A[k++]=arr2->A[j++];
+        else{
+            arr3->A[k++]=arr1->A[i++];
+            j++;
+        }
+    }
+    for(;i<arr1->length;i++)
+        arr3->A[k++]=arr1->A[i];
+    for(;j<arr2->length;j++)
+        arr3->A[k++]=arr2->A[j];
+
+    arr3->length=k;
+    arr3->size=10;
+
+    return arr3;
+}
+
+struct Array* Intersection(struct Array *arr1,struct Array
+*arr2){
+    int i,j,k;
+    i=j=k=0;
+    
+    struct Array *arr3=(struct Array *)malloc(sizeof(struct
+Array));
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i]<arr2->A[j])
+            i++;
+        else if(arr2->A[j]<arr1->A[i])
+            j++;
+        else if(arr1->A[i]==arr2->A[j]){
+            arr3->A[k++]=arr1->A[i++];
+            j++;
+        }
+    }
+
+    arr3->length=k;
+    arr3->size=10;
+
+    return arr3;
+}
+
+struct Array* Difference(struct Array *arr1,struct Array
+*arr2){
+    int i,j,k;
+    i=j=k=0;
+
+    struct Array *arr3=(struct Array *)malloc(sizeof(struct
+Array));
+
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i]<arr2->A[j])
+            arr3->A[k++]=arr1->A[i++];
+        else if(arr2->A[j]<arr1->A[i])
+            j++;
+        else{
+            i++;
+            j++;
+        }
+    }
+    for(;i<arr1->length;i++)
+        arr3->A[k++]=arr1->A[i];
+
+
+    arr3->length=k;
+    arr3->size=10;
+    return arr3;
+}
+
+
+
+
 int main(){
     struct Array arr;
     int n,i;
